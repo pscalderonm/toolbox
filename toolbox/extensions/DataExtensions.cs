27eec4690078141b0b4b85c3fb1882d94d45d942
Pablo S. Calderon <pscalderonm@gmail.com>
2018-03-28 11:27:44 -0500

    - Add a new data extensions - Add a .gitignore file

---
+++ toolbox/extensions/DataExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data;
+
+namespace com.pscalderonm.toolbox.extensions
+{
+	public static class DataExtensions
+	{
+		public static T? GetNul<T>(this IDataReader reader, string columnName) where T : struct
+		{
+			return reader[columnName] == DBNull.Value
+			? null
+			: new T?((T)Convert.ChangeType(reader[columnName], typeof(T)));
+		}
+
+		public static T Get<T>(this IDataReader reader, string columnName)
+		{
+			return reader[columnName] == DBNull.Value
+			? default(T)
+			: (T)Convert.ChangeType(reader[columnName], typeof(T));
+		}
+
+		public static string Get(this IDataReader reader, string columnName)
+		{
+			return Get<string>(reader, columnName);
+		}
+	}
+}
