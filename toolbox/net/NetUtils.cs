b8eb74b7c8f39f568157f6d4174e14723b3bf780
Pablo S. Calderon <pscalderonm@gmail.com>
2018-04-26 09:29:15 -0500

    - Adding NetUtils class

---
+++ toolbox/net/NetUtils.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+
+namespace com.pscalderonm.toolbox.net {
+	public static class NetUtils
+    {
+		public static string GetPublicIPv4Address() {
+			var address = IPAddress.Parse(new WebClient().DownloadString("http://icanhazip.com"));
+			return address?.MapToIPv4().ToString() ?? IPAddress.Loopback.MapToIPv4().ToString();			
+		}
+	}
+}
