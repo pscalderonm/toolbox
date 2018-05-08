854c91c58861ecf5684fc8bcdfac22f52e728ece
Pablo S. Calderon <pscalderonm@gmail.com>
2018-05-08 11:58:37 -0500

    - Support for auth requests

--- toolbox/api/client/RestApiBuilder.cs
+++ toolbox/api/client/RestApiBuilder.cs
@@ -5,42 +5,37 @@ using System.IO;
 using System.Net;
 using System.Threading.Tasks;
 
-namespace com.pscalderonm.toolbox.api.client
-{
-	public class RestApiBuilder
-	{
+namespace com.pscalderonm.toolbox.api.client {
+	public class RestApiBuilder {
 		private string uri;
 		private string method;
 		private string body;
 		private string contentType;
 		private int? timeout;
+		private ICredentials credentials;
 
 		const string JsonContentType = "application/json";
 
 		static readonly JsonSerializerSettings JsonSettings = new JsonSerializerSettings {
 			Culture = CultureInfo.GetCultureInfo("en-US"),
-			Formatting = Formatting.None			
+			Formatting = Formatting.None
 		};
 
-		public static RestApiBuilder Create()
-		{
+		public static RestApiBuilder Create() {
 			return new RestApiBuilder();
 		}
 
-		private RestApiBuilder()
-		{
+		private RestApiBuilder() {
 			method = "GET";
 			contentType = null;
 		}
 
-		public RestApiBuilder SetUri(string uri)
-		{
+		public RestApiBuilder SetUri(string uri) {
 			this.uri = uri;
 			return this;
 		}
 
-		public RestApiBuilder IsPost()
-		{
+		public RestApiBuilder IsPost() {
 			method = "POST";
 			return this;
 		}
@@ -55,44 +50,47 @@ namespace com.pscalderonm.toolbox.api.client
 			return this;
 		}
 
-		public RestApiBuilder SetBody(string body)
-		{			
+		public RestApiBuilder SetBody(string body) {
 			this.body = body;
 			return this;
 		}
 
-		public RestApiBuilder SetJsonBody(object body, JsonSerializerSettings jsonSerializerSettings=null)
-		{
+		public RestApiBuilder SetJsonBody(object body, JsonSerializerSettings jsonSerializerSettings = null) {
 			contentType = JsonContentType;
 			SetBody(jsonSerializerSettings != null ? JsonConvert.SerializeObject(body, jsonSerializerSettings) : JsonConvert.SerializeObject(body));
 			return this;
 		}
 
-		public RestApiBuilder SetTimeout(int timeout)
-		{
+		public RestApiBuilder SetTimeout(int timeout) {
 			this.timeout = timeout;
 			return this;
 		}
 
-		public T Call<T>(JsonSerializerSettings jsonDeserializerSettings=null)
-		{
+		public RestApiBuilder SetCredentials(string user, string password, string domain = "") {
+			SetCredentials(new NetworkCredential(user, password, domain));
+			return this;
+		}
+
+		public RestApiBuilder SetCredentials(ICredentials credentials) {
+			this.credentials = credentials;
+			return this;
+		}
+
+		public T Call<T>(JsonSerializerSettings jsonDeserializerSettings = null) {
 			var webRequest = WebRequest.Create(uri);
 			webRequest.Method = method;
 			webRequest.ContentType = contentType ?? webRequest.ContentType;
+			webRequest.Credentials = credentials ?? webRequest.Credentials;
 			webRequest.CachePolicy = new System.Net.Cache.RequestCachePolicy(System.Net.Cache.RequestCacheLevel.NoCacheNoStore);
 			webRequest.Timeout = timeout.HasValue ? timeout.Value : webRequest.Timeout;
-			if (IsBodyAllowed(webRequest))
-			{
-				using (var writer = new StreamWriter(webRequest.GetRequestStream()))
-				{
+			if (IsBodyAllowed(webRequest)) {
+				using (var writer = new StreamWriter(webRequest.GetRequestStream())) {
 					writer.WriteLine(body);
 				}
 			}
 
-			using (var webResponse = webRequest.GetResponse())
-			{
-				using (var reader = new StreamReader(webResponse.GetResponseStream()))
-				{
+			using (var webResponse = webRequest.GetResponse()) {
+				using (var reader = new StreamReader(webResponse.GetResponseStream())) {
 					string json = reader.ReadToEnd();
 					if (typeof(string) == typeof(T)) return (T)Convert.ChangeType(json, typeof(T));
 					else return JsonConvert.DeserializeObject<T>(json, jsonDeserializerSettings ?? JsonSettings);
@@ -100,34 +98,44 @@ namespace com.pscalderonm.toolbox.api.client
 			}
 		}
 
-		public async Task<T> CallAsync<T>(JsonSerializerSettings jsonDeserializerSettings = null)
-		{
+		public async Task<T> CallAsync<T>(JsonSerializerSettings jsonDeserializerSettings = null) {
 			var webRequest = WebRequest.Create(uri);
 			webRequest.Method = method;
 			webRequest.ContentType = contentType ?? webRequest.ContentType;
+			webRequest.Credentials = credentials ?? webRequest.Credentials;
 			webRequest.CachePolicy = new System.Net.Cache.RequestCachePolicy(System.Net.Cache.RequestCacheLevel.NoCacheNoStore); webRequest.Timeout = timeout.HasValue ? timeout.Value : webRequest.Timeout;
-			if (IsBodyAllowed(webRequest))
-			{
-				using (var writer = new StreamWriter(await webRequest.GetRequestStreamAsync()))
-				{
+			if (IsBodyAllowed(webRequest)) {
+				using (var writer = new StreamWriter(await webRequest.GetRequestStreamAsync())) {
 					await writer.WriteLineAsync(body);
-				}				
+				}
 			}
-
-			using (var webResponse = await webRequest.GetResponseAsync())
-			{				
-				using (var reader = new StreamReader(webResponse.GetResponseStream()))
-				{
+			
+			using (var webResponse = await webRequest.GetResponseAsync()) {
+				using (var reader = new StreamReader(webResponse.GetResponseStream())) {
 					string json = reader.ReadToEnd();
 					if (typeof(string) == typeof(T)) return (T)Convert.ChangeType(json, typeof(T));
 					else return JsonConvert.DeserializeObject<T>(json, jsonDeserializerSettings ?? JsonSettings);
-				} 
+				}
 			}
-		}		
+		}
 
-		public async Task<string> CallAsync(JsonSerializerSettings jsonDeserializerSettings = null)
-		{
+		public async Task<string> CallAsync(JsonSerializerSettings jsonDeserializerSettings = null) {
 			return await CallAsync<string>(jsonDeserializerSettings);
+		}
+
+		public async Task CallAsync() {
+			var webRequest = WebRequest.Create(uri);
+			webRequest.Method = method;
+			webRequest.ContentType = contentType ?? webRequest.ContentType;
+			webRequest.Credentials = credentials ?? webRequest.Credentials;
+			webRequest.CachePolicy = new System.Net.Cache.RequestCachePolicy(System.Net.Cache.RequestCacheLevel.NoCacheNoStore); webRequest.Timeout = timeout.HasValue ? timeout.Value : webRequest.Timeout;
+			if (IsBodyAllowed(webRequest)) {
+				using (var writer = new StreamWriter(await webRequest.GetRequestStreamAsync())) {
+					await writer.WriteLineAsync(body);
+				}
+			}
+
+			await webRequest.GetResponseAsync();			
 		}
 
 		private static bool IsBodyAllowed(WebRequest req) {
