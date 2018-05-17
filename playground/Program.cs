5cf8417549ac65014cea700793e9337d026b64c0
Pablo S. Calderon <pscalderonm@gmail.com>
2018-05-17 09:44:21 -0500

    - Fix issue when querying public ip address from an external service and its returned format is not valid. - Add a new console app for lib testing (The easiest way.)

---
+++ playground/Program.cs
@@ -0,0 +1,13 @@
+﻿using com.pscalderonm.toolbox.net;
+using System;
+
+namespace playground {
+	class Program {
+		static void Main(string[] args) {
+
+			Console.WriteLine(NetUtils.GetPublicIPv4Address());
+
+			Console.ReadKey();
+		}
+	}
+}
--- toolbox/net/NetUtils.cs
+++ toolbox/net/NetUtils.cs
@@ -1,11 +1,22 @@
 ﻿using System.Net;
+using System.Text.RegularExpressions;
 
 namespace com.pscalderonm.toolbox.net {
-	public static class NetUtils
-    {
+	public static class NetUtils {
+
+		static readonly Regex IPRegex = new Regex(@"((\d{1,3}\.?){4})");
+
 		public static string GetPublicIPv4Address() {
-			var address = IPAddress.Parse(new WebClient().DownloadString("http://icanhazip.com"));
-			return address?.MapToIPv4().ToString() ?? IPAddress.Loopback.MapToIPv4().ToString();			
+			string ipAddress = new WebClient().DownloadString("http://icanhazip.com");
+			var match = IPRegex.Match(ipAddress);
+			if (!match.Success) return string.Empty;
+			var address = IPAddress.Parse(match.Result("$1"));
+			return address?.MapToIPv4().ToString() ?? IPAddress.Loopback.MapToIPv4().ToString();
+		}
+
+		public static bool TryGetPublicIPv4Address(out string ipV4Address) {
+			ipV4Address = GetPublicIPv4Address();
+			return !string.IsNullOrEmpty(ipV4Address);
 		}
 	}
 }
